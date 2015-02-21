1f49cb3b1705e129b5fb902c362e3bf35d806681
parthpankajtiwary <parthpankajtiwary@gmail.com>
2015-02-22 00:52:10 +0530

    HANGOVER C++

--- hangover.cpp
+++ hangover.cpp
@@ -4,5 +4,17 @@
 using namespace std;
 
 int main() {
-
+    float input;
+    cin>>input;
+    while(input > 0.00) {
+        int n = 1;
+        float hang = 0.0;
+        while(hang < input) {
+            hang += 1.0/(n+1);
+            n += 1;
+        }
+            cout<<n-1<<" card(s)"<<endl;
+            cin>>input;
+    }
 }
+
