c7359d6d20d7c5fd4f600fcd10034c4c4f47fa10
parthpankajtiwary <parthpankajtiwary@gmail.com>
2015-03-08 22:24:08 +0530

    Tex Quotes UVa c++

--- UVa/texQuotes.cpp
+++ UVa/texQuotes.cpp
@@ -3,7 +3,24 @@
 using namespace std;
 
 int main() {
-    char input[200];
-    cin.getline(input, sizeof(input));
-    cout<<input<<endl;
+    char str[100];
+    cin.getline(str, sizeof(str));
+    int flag = 0;
+    while(cin) {
+        for(int i = 0; str[i] != '\0'; i++) {
+            if(str[i] == 34 && flag == 0) {
+                cout<<"\`\`";
+                flag = 1;
+            }
+            else if(str[i] == 34 && flag == 1) {
+                cout<<"\'\'";
+                flag = 0;
+            }
+            else {
+                cout<<str[i];
+            }
+        }
+        cout<<endl;
+        cin.getline(str, sizeof(str));
+    }
 }
